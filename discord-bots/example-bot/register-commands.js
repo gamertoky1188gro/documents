ff247be78ac2a580e24c6d846567fc8c6524fc55
gamertoky1188gro <tokyintelligentgamer@gmail.com>
2023-12-27 20:19:01 +0600

    pls allah

---
+++ discord-bots/example-bot/register-commands.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'meow',
+        description: 'reply with emoji'
+    },
+    {
+        name: 'date',
+        description: 'show date and time'
+    },
+    {
+        name: 'calculate',
+        description: 'plus calculate two number',
+        options: [
+            {
+                name: 'first-number',
+                description: 'enter first number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+                choices: [
+                    {
+                        name: '600',
+                        value: 600
+                    },
+                    {
+                        name: '1200',
+                        value: 1200
+                    }
+                ]
+            },
+            {
+                name: 'second-number',
+                description: 'enter second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+                choices: [
+                    {
+                        name: '600',
+                        value: 600
+                    },
+                    {
+                        name: '400',
+                        value: 400
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'gamertoky1188-yt-channel',
+        description: 'show gamertoky1188 youtube channel'
+    }
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('regestaring slash command...............')
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        )
+        console.log('slash command registared')
+    } catch (error) {
+        console.log(`⚠️there was an error⚠️:⚠️${error}⚠️`);
+    }
+})();
